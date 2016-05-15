68e16f7fb06fdad38ff80832aea105245691c504
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2016-05-15 19:59:42 +0530

    Defining EC2 error abstraction and signing implementation

---
+++ lib/internal/aws/ec2/sign.go
@@ -0,0 +1,37 @@
+package ec2
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"github.com/scorelab/gocloud/lib/internal/aws"
+	"sort"
+	"strings"
+)
+
+var b64 = base64.StdEncoding
+
+func sign(auth aws.Auth, method, path string, params map[string]string, host string) {
+	params["AWSAccessKeyId"] = auth.AccessKey
+	params["SignatureVersion"] = "2"
+	params["SignatureMethod"] = "HmacSHA256"
+	if auth.Token() != "" {
+		params["SecurityToken"] = auth.Token()
+	}
+
+	var keys, sarray []string
+	for k, _ := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		sarray = append(sarray, aws.Encode(k) + "=" + aws.Encode(params[k]))
+	}
+	joined := strings.Join(sarray, "&")
+	payload := method + "\n" + host + "\n" + path + "\n" + joined
+	hash := hmac.New(sha256.New, []byte(auth.SecretKey))
+	hash.Write([]byte(payload))
+	signature := make([]byte, b64.EncodedLen(hash.Size()))
+	b64.Encode(signature, hash.Sum(nil))
+	params["Signature"] = string(signature)
+}
