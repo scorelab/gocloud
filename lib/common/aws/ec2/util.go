9a35fd67aafe641479fbe32f16f3fe08002c8f4c
Tharindu Munasinghe <munasinghetharindu@gmail.com>
2016-07-27 21:11:51 +0530

    add the util functions into ec2/util

---
+++ lib/common/aws/ec2/util.go
@@ -0,0 +1,37 @@
+package ec2
+
+
+import (
+	"net/url"
+	"net/http"
+	"encoding/xml"
+)
+
+func multimap(p map[string]string) url.Values {
+	q := make(url.Values, len(p))
+	for k, v := range p {
+		q[k] = []string{v}
+	}
+	return q
+}
+
+func buildError(r *http.Response) error {
+	errors := xmlErrors{}
+	xml.NewDecoder(r.Body).Decode(&errors)
+	var err Error
+	if len(errors.Errors) > 0 {
+		err = errors.Errors[0]
+	}
+	err.RequestId = errors.RequestId
+	err.StatusCode = r.StatusCode
+	if err.Message == "" {
+		err.Message = r.Status
+	}
+	return &err
+}
+
+func makeParams(action string) map[string]string {
+	params := make(map[string]string)
+	params["Action"] = action
+	return params
+}
